76293794ac2a66293c420599661b0e21f6455dbb
psayeed1990 <sayeedmondal1412@gmail.com>
2020-08-13 10:00:25 +0600

    client add react

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>copyright &copy;Haylingprint.co.uk {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
---
+++ client/src/components/layout/navbar/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './navbar.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Contact</li>
+      </ul>
+    );
+  }
+}
+
+export default Navbar;

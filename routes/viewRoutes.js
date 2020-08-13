6bc2f5a2c3fc9e7666d03f73a33bdad887407326
psayeed1990 <sayeedmondal1412@gmail.com>
2020-08-13 17:06:32 +0600

    login register form done

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,4 +4,23 @@ router.get('/', (req, res) => {
   res.render('home');
 });
 
+router.get('/register', (req, res) => {
+  res.render('auth/register');
+});
+
+router.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+router.get('/cart', (req, res) => {
+  res.render('cart');
+});
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/account', (req, res) => {
+  res.render('user/account');
+});
+
 module.exports = router;

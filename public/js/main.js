91b1cd136b8cb90a6d945775af7762f474ef7805
psayeed1990 <sayeedmondal1412@gmail.com>
2020-08-26 21:14:49 +0600

    product variable done

---
+++ public/js/main.js
@@ -0,0 +1,132 @@
+//starts code for product variable
+window.onload = function () {
+  if (document.getElementById('variants')) {
+    let variants = JSON.parse(document.getElementById('variants').innerHTML);
+    let variableForm = document.getElementById('variable-form');
+    let baseName = document.getElementById('base-name');
+    let price = document.querySelector('span#price');
+    let stock = document.querySelector('span#stock');
+    let SKU = document.getElementById('sku');
+    let quantity = document.getElementById('quantity');
+
+    let baseVariants = variants.filter((variant) => variant.base === '');
+
+    baseVariants.forEach((variant) => {
+      baseName.innerHTML = variant.name + ': ';
+      variableForm.innerHTML += `<input class="base-radio" style="width: 40px; display: inline;" type="radio" name="baseVariableName" value="${variant.value}" required="true"><label style="width: 40px; display: inline;">${variant.value}</label>`;
+    });
+
+    let baseRadioClass = document.getElementsByClassName('base-radio');
+    for (let i = 0; i < baseRadioClass.length; i++) {
+      baseRadioClass[i].addEventListener('click', changeContent);
+
+      function changeContent() {
+        price.innerHTML = baseVariants[i].price;
+        stock.innerHTML = baseVariants[i].stock;
+        sku.value = baseVariants[i].SKU;
+        quantity.max = baseVariants[i].stock;
+
+        let secondLevel = variants.filter(
+          (variant) => variant.base === baseVariants[i].SKU
+        );
+
+        if (secondLevel.length === 0) {
+          document.getElementById('secondLevel-form').style.display = 'none';
+          document.getElementById('thirdLevel-form').style.display = 'none';
+        }
+        if (secondLevel.length > 0) {
+          document.getElementById('secondLevel-form').style.display = 'block';
+          document
+            .getElementsByName('secondVariableName')
+            .forEach((sec) => (sec.checked = false));
+        }
+      }
+    }
+
+    //second level
+    for (let i = 0; i < baseRadioClass.length; i++) {
+      let secondLevel = variants.filter(
+        (variant) => variant.base === baseVariants[i].SKU
+      );
+      if (secondLevel.length > 0) {
+        let secondLevelForm = document.getElementById('secondLevel-form');
+        let secondLevelName = document.getElementById('secondLevel-name');
+
+        secondLevel.forEach((variant) => {
+          secondLevelName.innerHTML = variant.name + ': ';
+          secondLevelForm.innerHTML += `<input class="second-radio" style="width: 40px; display: inline;" type="radio" name="secondVariableName" value="${variant.value}" required="true"><label style="width: 40px; display: inline;">${variant.value}</label>`;
+        });
+
+        let secondRadioClass = document.getElementsByClassName('second-radio');
+
+        for (let j = 0; j < secondRadioClass.length; j++) {
+          secondRadioClass[j].addEventListener('click', changeSecContent);
+
+          function changeSecContent() {
+            price.innerHTML = secondLevel[j].price;
+            stock.innerHTML = secondLevel[j].stock;
+            sku.value = secondLevel[j].SKU;
+            quantity.max = secondLevel[j].stock;
+
+            let thirdLevel = variants.filter(
+              (variant) => variant.base === secondLevel[j].SKU
+            );
+
+            if (thirdLevel.length === 0) {
+              document.getElementById('thirdLevel-form').style.display = 'none';
+            }
+            if (thirdLevel.length > 0) {
+              document.getElementById('thirdLevel-form').style.display =
+                'block';
+              document
+                .getElementsByName('thirdVariableName')
+                .forEach((sec) => (sec.checked = false));
+            }
+          }
+        }
+      }
+    }
+
+    //third level
+    for (let i = 0; i < baseRadioClass.length; i++) {
+      let secondLevel = variants.filter(
+        (variant) => variant.base === baseVariants[i].SKU
+      );
+      if (secondLevel.length > 0) {
+        let secondRadioClass = document.getElementsByClassName('second-radio');
+
+        for (let j = 0; j < secondRadioClass.length; j++) {
+          let thirdLevel = variants.filter(
+            (variant) => variant.base === secondLevel[j].SKU
+          );
+
+          if (thirdLevel.length > 0) {
+            let thirdLevelForm = document.getElementById('thirdLevel-form');
+            let thirdLevelName = document.getElementById('thirdLevel-name');
+
+            thirdLevel.forEach((variant) => {
+              thirdLevelName.innerHTML = variant.name + ': ';
+              thirdLevelForm.innerHTML += `<input class="third-radio" style="width: 40px; display: inline;" type="radio" name="thirdVariableName" value="${variant.value}" required><label style="width: 40px; display: inline;">${variant.value}</label>`;
+            });
+
+            let thirdRadioClass = document.getElementsByClassName(
+              'third-radio'
+            );
+
+            for (let k = 0; k < thirdRadioClass.length; k++) {
+              thirdRadioClass[k].addEventListener('click', changeThirdContent);
+
+              function changeThirdContent() {
+                price.innerHTML = thirdLevel[k].price;
+                stock.innerHTML = thirdLevel[k].stock;
+                sku.value = thirdLevel[k].SKU;
+                quantity.max = thirdLevel[k].stock;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+//ends code for product variable

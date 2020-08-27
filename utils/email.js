3db19ac47d42724d0e31f50985dd0383b6602f8d
psayeed1990 <sayeedmondal1412@gmail.com>
2020-08-27 18:27:33 +0600

    email address

--- utils/email.js
+++ utils/email.js
@@ -6,7 +6,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Abu Sayeed <${process.env.EMAIL_FROM}>`;
+    this.from = `Hayling Print <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
@@ -21,14 +21,14 @@ module.exports = class Email {
       });
     }
 
-    // return nodemailer.createTransport({
-    //   host: process.env.EMAIL_HOST,
-    //   port: process.env.EMAIL_PORT,
-    //   auth: {
-    //     user: process.env.EMAIL_USERNAME,
-    //     pass: process.env.EMAIL_PASSWORD
-    //   }
-    // });
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
   }
 
   // Send the actual email
